5801507ed1dd015a5bdc982df380f1cdbc61abb7
Shyam Chen <shyamchen1994@gmail.com>
2017-03-23 13:59:10 +0800

    Update

--- index.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-
-http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Little Books');
-}).listen(8888);
-
-console.log('Server running at http://localhost:8888/.');
---
+++ index.ts
@@ -0,0 +1,1 @@
+console.log('Welcome to Big Little Books.');
