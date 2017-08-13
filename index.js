aa11ecd76cddf1bfbc991d5084e78ad4b980e2e0
Shyam Chen <shyamchen1994@gmail.com>
2017-08-13 15:53:47 +0800

    Update

--- index.js
+++ index.js
@@ -2,20 +2,4 @@
  * $ node index.js
  */
 
-// console.log('Welcome to Big Little Books.');
-
-const bar = () => 'Bar';
-
-const foo = async () => {
-  const result = await bar();
-
-  if (result) {
-    return `Foo, ${result}`;
-  } else {
-    throw new Error('Problem!');
-  }
-};
-
-foo()
-  .then(value => console.log(value))
-  .catch(error => console.error(error));
+console.log('Welcome to Big Little Books.');
